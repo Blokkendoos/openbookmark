0064118159658a2cf897023b2ecd5f3c1070e6de
blokkendoos <jvo@chaosgeordend.nl>
2020-11-27 21:15:16 +0100

    Detab and follow (as much as possible) the PSR

--- bookmark_verify.php
+++ bookmark_verify.php
@@ -1,78 +1,104 @@
 <?php
-require_once ("./header.php");
-logged_in_only ();
+/**
+ PHP Grab favicon
 
-$bmlist = set_get_num_list ('bmlist');
+ @category  Bookmarks
+ @package   Openbookmark
+ @author    J. van Oostrum <jvo@chaosgeordend.nl>
+ @copyright 2010 Brendan LaMarche
+ @license   GNU General Public License version 2
+ @link      https://github.com/blamarche/openbookmark
+ @link      https://github.com/blokkendoos/openbookmark
+ */
+require_once "./header.php";
+logged_in_only();
 
-if (count ($bmlist) > 0) {
-	?>
+$bmlist = set_get_num_list('bmlist');
 
-	<h2 class="title">Verify bookmarks:</h2>
-	<form action="<?php echo $_SERVER['SCRIPT_NAME'] . "?folderid=" . $folderid; ?>" method="POST" name="bookmarksverify">
+if (count($bmlist) > 0) {
+    ?>
+
+    <h2 class="title">Verify bookmarks:</h2>
+    <form action="
+    <?php
+    echo $_SERVER['SCRIPT_NAME'] . "?folderid=" . $folderid;
+    ?>"
+    method="POST" name="bookmarksverify">
 
         <?php
-        $qbmlist = implode (",", $bmlist);
-        $query = sprintf ("SELECT url, title FROM bookmark WHERE id IN (%s) AND user='%s'",
-            $mysql->escape ($qbmlist),
-            $mysql->escape ($username) );
+        $qbmlist = implode(',', $bmlist);
+        $query = sprintf(
+            "SELECT url, title FROM bookmark WHERE id IN(%s) AND user='%s'",
+            $mysql->escape($qbmlist),
+            $mysql->escape($username)
+        );
         ?>
         
         <table border=1>
-        <tr><th>Bookmark</th><th>HTML Status</th></tr>
+        <tr><th>Bookmark</th><th>HTTP Response</th></tr>
 
         <?php
-        if ($mysql->query ($query)) {
+        if ($mysql->query($query)) {
             while ($row = mysqli_fetch_row($mysql->result)) {
             
-       	        $htmlStatus = verifyUrl($row[0]);
-       	        if ($htmlStatus <> 200 AND $htmlStatus <> 301) {
-       	        ?>
+                $htmlResponse = verifyUrl($row[0]);
+                if ($htmlResponse <> 200 AND $htmlResponse <> 301) {
+                    ?>
 
                 <tr>
-					<td><a
-						class="bookmark_href"
-						target="_blank"
-						title="<? echo $row[1]; ?>"
-						href="<? echo $row[0]; ?>">
-						<? echo $row[1]; ?>
-						</a>
-					</td>
-					<td><? echo $htmlStatus; ?></td>
+                    <td><a
+                        class="bookmark_href"
+                        target="_blank"
+                        title="<? echo $row[1]; ?>"
+                        href="<? echo $row[0]; ?>">
+                        <? echo $row[1]; ?>
+                        </a>
+                    </td>
+                    <td><? echo $htmlResponse; ?></td>
                 </tr>
-				       
-            <?php	        
-				}
+                       
+                    <?php            
+                }
             }
-        }
-        else {
-            message ($mysql->error);
+        } else {
+            message($mysql->error);
         }
         ?>
 
         </table>
         <p>Bookmarks verified</p><br>
-		<input type="hidden" name="bmlist">
-		<input type="button" value=" Ok " onClick="self.close()">
+        <input type="hidden" name="bmlist">
+        <input type="button" value=" Ok " onClick="self.close()">
 
-	</form>
+    </form>
 
-	<script type="text/javascript">
-	document.bookmarksverify.bmlist.value = self.name;
-	</script>
+    <script type="text/javascript">
+    document.bookmarksverify.bmlist.value = self.name;
+    </script>
 
-	<?php
+    <?php
 }
 
-function verifyUrl($url) {
+/**
+ Verify URL,
+ i.e. check whether the content loads successfull (200) or not.
+
+ @param $url the URL to verify
+ 
+ @return the HTTP response code
+ */
+function verifyUrl($url)
+{
     $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch, CURLOPT_TIMEOUT,5);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,5);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
     curl_exec($ch);
-    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);  
-    return $code;
+    return $response;
 }
 
-require_once (ABSOLUTE_PATH . "footer.php");
+require_once ABSOLUTE_PATH . "footer.php";
 ?>
+
