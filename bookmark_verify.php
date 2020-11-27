120e0940969c7172cd8b4d896f677ef0df6f534f
blokkendoos <jvo@chaosgeordend.nl>
2020-11-27 22:22:07 +0100

    Fix HTTP response 500

--- bookmark_verify.php
+++ bookmark_verify.php
@@ -90,6 +90,9 @@ if (count($bmlist) > 0) {
 function verifyUrl($url)
 {
     $ch = curl_init($url);
+    // use an agent that is likely to be accepted by the host
+    $user_agent = "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0";
+    curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_TIMEOUT, 5);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
